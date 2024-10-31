b11ddb0ee912a4b6b138383a0b2e54f169fd2e97
paytoncampbell2 <paypay.cams19@gmail.com>
2024-10-31 18:24:13 -0400

    Put and Delete

--- buckeyebot.Api/CatalogController.cs
+++ buckeyebot.Api/CatalogController.cs
@@ -52,15 +52,32 @@ public IActionResult PostRating(int id, [FromBody] Rating rating)
     return Ok(item);
 }
 [HttpPut("{id;int}")]
-public IActionResult Put(int id, Item item)
+public IActionResult Put(int id, [FromBody]Item item)
 {
+    if (id != item.Id) {
+        return BadRequest();
+
+    }
+    if(_db.Items.Find(id) == null) {
+        return NotFound();
+    }
+    _db.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+    _db.SaveChanges();
+
     return NoContent();
 }
     
     [HttpDelete("{id:int}")]
     public IActionResult Delete(int id)
     {
-        return NoContent();
+        var item = _db.Items.Find(id);
+        if (item == null) {
+            return NotFound();
+        }
+        _db.Items.Remove(item);
+        _db.SaveChanges();
+
+        return Ok();
     }
 }
 
