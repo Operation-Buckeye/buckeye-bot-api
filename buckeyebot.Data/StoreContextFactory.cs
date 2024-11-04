e71b137e083cb83d5c54bf2bc6714ab006abe66e
paytoncampbell2 <paypay.cams19@gmail.com>
2024-11-03 21:22:11 -0500

    page 31

---
+++ buckeyebot.Data/StoreContextFactory.cs
@@ -0,0 +1,14 @@
+using buckeyebot.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace buckeyebot.Data;
+
+public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext> 
+{
+    public StoreContext CreateDbContext(string[] args) {
+        var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+        optionsBuilder.UseSqlite("Data Source = . . /Registrar.sqlite");
+        return new StoreContext(optionsBuilder.Options);
+    }
+}
